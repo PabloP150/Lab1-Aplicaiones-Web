ebe97a9bc9a53b936598f3e4492668d413954fc2
Pablo Pineda <pablo.pineda@galileo.edu>
2024-01-23 20:28:46 -0600

    3 new files

---
+++ suma.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+      int num1,num2,resultado;
+      num1=10;
+      num2=5;
+      resultado=num1+num2;
+      System.out.println("El resultado de la suma es: "+resultado);
+    }
+}
